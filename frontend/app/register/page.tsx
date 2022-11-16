411d1cb627e1cb8ff05a556e7cacc4362116a7d5
Dawidu7 <109083134+Dawidu7@users.noreply.github.com>
2022-11-16 19:26:11 +0100

    Input Component

--- frontend/app/register/page.tsx
+++ frontend/app/register/page.tsx
@@ -1,4 +1,8 @@
+'use client'
+
 import axios from 'axios'
+
+import Input from '../../components/Input'
 
 
 const Register = () => {
@@ -8,20 +12,38 @@ const Register = () => {
 
     const response = await axios.post(`${process.env.BACKEND_URL}/users/create`, {
       email: form.email.value,
-      username: form.userna
+      username: form.username.value
     })
+    .then(response => response)
+    .catch(error => error.response)
+  }
+
+  const getEmailError: (value: string) => string = (value: string) => {
+    if(value.length > 320) {
+      return 'Email too long'
+    }
+    else if(!value.match('[a-z0-9]+@[a-z]+\.[a-z]{2,3}')) {
+      return 'Invalid Email'
+    }
+
+    return ''
+  }
+
+  const getUsernameError: (value: string) => string = (value: string) => {
+    if(value.length < 3) {
+      return 'Username shorter than 3 charactes'
+    }
+    else if(value.length > 20) {
+      return 'Username longer than 20 characters'
+    }
+    
+    return ''
   }
   
   return (
     <form onSubmit={register}>
-      <div>
-        <label>Email</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Username</label>
-        <input type="text" name="username" />
-      </div>
+      <Input label='Email' type='text' getError={getEmailError} />
+      <Input label='Username' type='text' getError={getUsernameError} />
       <div>
         <label>Password</label>
         <input type="password" name="password" />
--- frontend/components/Input.tsx
+++ frontend/components/Input.tsx
@@ -1,24 +1,30 @@
 'use client'
 
-import React, { useState } from 'react'
+import { useState } from 'react'
 
 
 interface IInput {
   label: string,
-  type: string
+  type: string,
+  getError: (value: string) => string
 }
 
-const Input = ({ label, type } : IInput) => {
+const Input = ({ label, type, getError } : IInput) => {
   const [ value, setValue ] = useState('')
+  const [ error, setError ] = useState('')
 
   return (
     <div>
-      <label>{label}</label>
-      <input 
-        type={type} 
-        value={value} 
-        onChange={e => setValue(e.currentTarget.value)} 
-      />
+      <div className="flex justify-between gap-x-4 items-center">
+        {label && <label>{label}</label>}
+        <input 
+          type={type} 
+          value={value} 
+          onChange={e => {setValue(e.currentTarget.value); error !== '' && setError(getError(e.currentTarget.value)) }}
+          onBlur={e => setError(getError(e.currentTarget.value))}
+        />
+      </div>
+      {error !== '' && <p className='text-red-700 text-center'>{error}</p>}
     </div>
   )
 }
